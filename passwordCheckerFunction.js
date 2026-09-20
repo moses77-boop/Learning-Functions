4f21f1866914b991ac34dd15160f15a442986cf2
Precious Moses <mosesjeffrey74@gmail.com>
2026-09-20 21:38:51 +0100

    Add function and conditioning in passwordCheckerFunction.js

--- passwordCheckerFunction.js
+++ passwordCheckerFunction.js
@@ -1,9 +1,14 @@
 // Already have the password stored in a variable
 const password = "secretword123";
+
 // Receive the value which the user entered
-
+function checkPassword(userInput){
 // Compare the two values
-
+if (userInput === password){
 // If the match print "Correct password entered"
-
+console.log("Correct password entered");
+    } else {
 //  If they don't match print "Incorrect password, please try again"
+console.log("Incorrect password, please try again");
+            }
+}
